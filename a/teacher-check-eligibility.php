3994d4ba5374cda64e893bc0db7985f56b810bd6
“Jon <jonmaher@me.com>
2018-12-14 14:14:50 +0000

    d

--- a/teacher-check-eligibility.php
+++ a/teacher-check-eligibility.php
@@ -43,11 +43,10 @@
                 <h2 class="govuk-heading-l">About this scheme</h2>
 
                 <p class="govuk-body">From
-                    <?php echo $policy_criteria_start_date  ?>, teachers can claim back any student loan payments made through their wages between
-                    <?php echo $tax_year_start_date  ?> and
-                    <?php echo $tax_year_end_date  ?> (the last tax year).</p>
+                    <?php echo $policy_criteria_start_date  ?>, teachers can claim a bursary payment of
+                    <?php echo $total_retention_payment  ?>.</p>
                 <p class="govuk-body">The scheme is for teachers in certain subjects and is only running in certain areas of England – areas that have fewer teachers in these subjects at the moment.</p>
-                <p class="govuk-body">The payment you get will be in one lump sum, and you will not have to pay tax or National Insurance on it.</p>
+                <p class="govuk-body">The payments will be made as two amounts of £2000 in the Autumn terms of 2019/20 and 2020/21. You will not have to pay tax or National Insurance on it.</p>
 
                 <nav class="tslr-pagination" role="navigation">
                     <ul>
--- includes/variables.php
+++ includes/variables.php
@@ -3,4 +3,5 @@ $service_name = "Teachers: claim bursary payments";
 $policy_criteria_start_date = "2019";
 $tax_year_start_date = "6 April 2018";
 $tax_year_end_date = "5 April 2019";
+$total_retention_payment = "£4000";
 ?>

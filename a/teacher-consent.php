b2486625fff76e3c2d6bc5a63f1b911da162ae90
“Jon <jonmaher@me.com>
2018-12-14 15:27:53 +0000

    s

--- a/teacher-consent.php
+++ a/teacher-consent.php
@@ -14,8 +14,8 @@
 
 
     <div class="grid-container">
-        <div class="grid-x grid-padding-x">
-            <div class="large-8 cell">
+        <div id="main-content-area" class="grid-x grid-padding-x grid-margin-x">
+            <div id="main-content-left" class="large-8 cell">
                 <h1 class="govuk-heading-xl">Consent to us contacting your school</h1>
                 <p class="govuk-body">To claim your student loan payments back we will contact the school you tell us about in this claim to confirm:</p>
                 <ul class="govuk-list govuk-list--bullet">

85d83558cd64054bc31550fb8d6d1c035afd6f1c
“Jon <jonmaher@me.com>
2018-12-14 16:12:58 +0000

    Update teacher-location-eligibility.php

--- a/teacher-location-eligibility.php
+++ a/teacher-location-eligibility.php
@@ -60,7 +60,16 @@
                                 <div class="govuk-form-group">
                                     <input class="govuk-input" id="teacher-schools-setup" name="teacher-schools-setup" type="hidden" value="true">
                                     <label class="govuk-label" for="teacher-school-name">Enter the school name</label>
-                                    <input id="teacher-school-name" name="teacher-school-name" type="text" class="govuk-input awesomplete" data-list="Ada, Java, JavaScript, Brainfuck, LOLCODE, Node.js, Ruby on Rails" />
+                                    <input id="teacher-school-name" name="teacher-school-name" type="text" class="govuk-input awesomplete" data-list="#mylist" />
+                                    <ul id="mylist">
+                                        <li>Ada</li>
+                                        <li>Java</li>
+                                        <li>JavaScript</li>
+                                        <li>Brainfuck</li>
+                                        <li>LOLCODE</li>
+                                        <li>Node.js</li>
+                                        <li>Ruby on Rails</li>
+                                    </ul>
                                 </div>
 
                                 <div class="govuk-form-group">

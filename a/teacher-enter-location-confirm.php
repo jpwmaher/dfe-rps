6476712802d49cbb6b2758d928781d15687608aa
“Jon <jonmaher@me.com>
2018-12-17 10:27:09 +0000

    Update teacher-enter-location-confirm.php

--- a/teacher-enter-location-confirm.php
+++ a/teacher-enter-location-confirm.php
@@ -17,12 +17,28 @@
         <div id="main-content-area" class="grid-x grid-padding-x grid-margin-x">
             <div id="main-content-left-only" class="large-8 cell">
 
-                <form action="#" method="post" onsubmit="usePage(this, 'bob');">
-                    <input type="radio" name="bob" value="regtku1.htm">
-                    <input type="radio" name="bob" value="regtku2.htm">
-                    <input type="radio" name="bob" value="regtku3.htm">
-                    <input type="submit" value="Go">
+                <form onsubmit="return mysubmit();">
+                    <select id="option" name="Region">
+   <option value="value1.html">Option1</option>
+   <option value="value2.html">Option2</option>
+   <option value="value3.html">Option3</option>
+   <option value="value3.html">Option4</option>
+   <option value="value3.html">Option5</option>
+</select>
+
+                    <input type="submit" id="submit" value="Submit">
+
                 </form>
+
+                <script>
+                    function mysubmit() {
+                        //you can return false; here to prevent the form being submitted
+                        //useful if you want to validate the form
+
+                        window.location.href = document.getElementById('option').value;
+                    }
+
+                </script>
 
             </div>
             <div id="sidebar" class="large-4 cell no-top-border">

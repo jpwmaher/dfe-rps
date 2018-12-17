1b35fc66c818032b0daf960bd25dc139a3fcb36d
“Jon <jonmaher@me.com>
2018-12-17 10:35:42 +0000

    Update teacher-enter-location-confirm.php

--- a/teacher-enter-location-confirm.php
+++ a/teacher-enter-location-confirm.php
@@ -17,28 +17,16 @@
         <div id="main-content-area" class="grid-x grid-padding-x grid-margin-x">
             <div id="main-content-left-only" class="large-8 cell">
 
-                <form onsubmit="return mysubmit();">
-                    <select id="option" name="Region">
-   <option value="www.google.com">Option1</option>
-   <option value="www.amazon.co.uk">Option2</option>
-   <option value="www.apple.com">Option3</option>
-   <option value="www.google.com">Option4</option>
-   <option value="value3.html">Option5</option>
+                <form>
+                    <select id="menu">
+  <option selected="selected">Select One</option>
+  <option value="http://www.domain-one.com">London</option>
+  <option value="http://www.domain-two.com">Glasgow</option>
 </select>
-
-                    <input type="submit" id="submit" value="Submit">
+                    <input type="button" id="goBtn" value="GO!">
 
                 </form>
 
-                <script>
-                    function mysubmit() {
-                        //you can return false; here to prevent the form being submitted
-                        //useful if you want to validate the form
-
-                        window.location.href = 'http://' + document.getElementById('option').value;
-                    }
-
-                </script>
 
             </div>
             <div id="sidebar" class="large-4 cell no-top-border">

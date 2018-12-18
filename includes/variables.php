2325914afec26b267ef139cf9d8420057177f1cb
“Jon <jonmaher@me.com>
2018-12-18 11:05:06 +0000

    s

--- includes/variables.php
+++ includes/variables.php
@@ -4,6 +4,6 @@ $policy_criteria_start_date = "2019";
 $tax_year_start_date = "6 April 2018";
 $tax_year_end_date = "5 April 2019";
 $total_retention_payment = "£4000";
-$qualification_date = "1 September 2016";
+$qualification_date = "1 September 2014";
 $form_generic_error_message = "There's a problem"
 ?>

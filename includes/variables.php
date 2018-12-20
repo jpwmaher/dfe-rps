101ad6c0e607391f336eaadf1811e0651ffc5ed1
“Jon <jonmaher@me.com>
2018-12-20 09:26:54 +0000

    Update variables.php

--- includes/variables.php
+++ includes/variables.php
@@ -1,8 +1,8 @@
 <?php 
 $service_name = "Teachers: claim bursary payments";
 $policy_criteria_start_date = "2019";
-$academic_year_start_date = "6 April 2018";
-$academic_year_end_date = "5 April 2019";
+$academic_year_start_date = "1st September 2017";
+$academic_year_end_date = "30 June 2018";
 $total_retention_payment = "£4000";
 $qualification_date = "1 September 2014";
 $form_generic_error_message = "There's a problem"

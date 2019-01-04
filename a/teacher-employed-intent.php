f4c1c1adac85834271c18d365bdedbb9bbd1f77e
“Jon <jonmaher@me.com>
2019-01-04 12:44:40 +0000

    Update teacher-employed-intent.php

--- a/teacher-employed-intent.php
+++ a/teacher-employed-intent.php
@@ -106,7 +106,7 @@
 
                                             <!-- <span id="teacher-trn-hint" class="govuk-hint">This is on the certificate you got when you qualified as a teacher, or your school can tell you.</span> -->
 
-                                            <input class="govuk-input " id="teacher-ni" name="teacher-ni" type="text" value="" required pattern="alpha_numeric">
+                                            <input class="govuk-input " id="teacher-ni" name="teacher-ni" type="text" value="" pattern="alpha_numeric">
 
                                             <br/>
                                         </div>

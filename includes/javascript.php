e08d8a06f544228a8641d777c3f6b4920f751768
“Jon <jonmaher@me.com>
2019-01-07 08:58:02 +0000

    Update javascript.php

--- includes/javascript.php
+++ includes/javascript.php
@@ -3,24 +3,3 @@
 <script src="/js/vendor/what-input.js"></script>
 <script src="/js/vendor/foundation.js"></script>
 <script src="/js/app.js"></script>
-<script>
-    function myCustomValidator(
-        $el, /* jQuery element to validate */
-        required, /* is the element required according to the `[required]` attribute */
-        parent /* parent of the jQuery element `$el` */
-    ) {
-        if (!required) return true;
-        var from = $('#' + $el.attr('data-greater-than')).val(),
-            to = $el.val();
-        return (parseInt(to) > parseInt(from));
-    };
-
-    // Set default options
-    Foundation.Abide.defaults.patterns['dashes_only'] = /^[0-9-]*$/;
-    Foundation.Abide.defaults.patterns['letters_spaces'] = /^[a-z\d\-_\s]+$/i;
-    Foundation.Abide.defaults.validators['greater_than'] = myCustomValidator;
-
-    // Initialize Foundation
-    $(document).foundation();
-
-</script>

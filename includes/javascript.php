1b3d0c2765343bffcff89a5f5a5b4f25477dc8de
“Jon <jonmaher@me.com>
2019-01-07 08:54:42 +0000

    s

--- includes/javascript.php
+++ includes/javascript.php
@@ -4,12 +4,23 @@
 <script src="/js/vendor/foundation.js"></script>
 <script src="/js/app.js"></script>
 <script>
-    $(document).foundation({
-        abide: {
-            patterns: {
-                letters_and_spaces: /^[\w\-\s]+$/
-            }
-        }
-    });
+    function myCustomValidator(
+        $el, /* jQuery element to validate */
+        required, /* is the element required according to the `[required]` attribute */
+        parent /* parent of the jQuery element `$el` */
+    ) {
+        if (!required) return true;
+        var from = $('#' + $el.attr('data-greater-than')).val(),
+            to = $el.val();
+        return (parseInt(to) > parseInt(from));
+    };
+
+    // Set default options
+    Foundation.Abide.defaults.patterns['dashes_only'] = /^[0-9-]*$/;
+    Foundation.Abide.defaults.patterns['letters_spaces'] = /^[a-z\d\-_\s]+$/i;
+    Foundation.Abide.defaults.validators['greater_than'] = myCustomValidator;
+
+    // Initialize Foundation
+    $(document).foundation();
 
 </script>

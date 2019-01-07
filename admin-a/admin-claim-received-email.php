da9ab8bf43a3ef2b9ce0071ea86868f139cce0ea
“Jon <jonmaher@me.com>
2019-01-07 09:54:15 +0000

    s

---
+++ admin-a/admin-claim-received-email.php
@@ -0,0 +1,50 @@
+<?php ?>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+
+<?php include("../includes/head.php"); ?>
+
+<?php include("../includes/variables.php"); ?>
+
+<body>
+    <!-- Masthead -->
+    <!--  <?php include("../includes/masthead.php"); ?> -->
+
+    <!-- Alpha banner -->
+    <!--  <?php include("../includes/alpha-banner.php"); ?> -->
+
+
+    <div class="grid-container">
+        <div id="main-content-area" class="grid-x grid-padding-x grid-margin-x">
+            <div id="main-content-left-only" class="large-8 cell callout">
+
+
+                <h2 class="govuk-heading-s">Subject</h2>
+                <p class="govuk-body">Jane Jones: claim for repayment of student loan</p>
+                <h2 class="govuk-heading-s">Body</h2>
+                <p class="govuk-body">Dear [name]</p>
+                <p class="govuk-body">Jane Jones has applied to claim back their student loan payments.</p>
+                <p class="govuk-body">To process the claim, we need you to check what the teacher has told us about:</p>
+                <ul class="govuk-list govuk-list--bullet">
+                    <li>the subject they taught</li>
+                    <li>how much of their time was spent teaching that subject</li>
+                    <li>how much student loan they paid back through their wages</li>
+                </ul>
+                <p class="govuk-body">Check this information before {date}. If you cannot, {do something}.</p>
+                <p class="govuk-body"><a href="admin-dfe-signin">Sign in to DfE</a> to check Jane Jones’s details.</p>
+                <p class="govuk-body">Kind regards</p>
+                <p class="govuk-body">{Service name}</p>
+
+
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Footer -->
+    <?php include("../includes/footer.php"); ?>
+
+    <?php include("../includes/javascript.php"); ?>
+</body>
+
+</html>

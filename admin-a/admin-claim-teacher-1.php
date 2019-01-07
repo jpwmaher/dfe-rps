420775dadf621f25e4e9544ca727b67b7f7f1bce
“Jon <jonmaher@me.com>
2019-01-07 14:18:00 +0000

    Update admin-claim-teacher-1.php

--- admin-a/admin-claim-teacher-1.php
+++ admin-a/admin-claim-teacher-1.php
@@ -1,178 +1,171 @@
 <?php ?>
 
-<?php
-$worked = isset($_POST['worked']) ? $_POST['worked'] : 'Incomplete';
-?>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
 
-    <!doctype html>
-    <html class="no-js" lang="en" dir="ltr">
+<?php include("../includes/head.php"); ?>
 
-    <?php include("../includes/head.php"); ?>
+<?php include("../includes/variables.php"); ?>
 
-    <?php include("../includes/variables.php"); ?>
+<body>
+    <!-- Masthead -->
+    <?php include("../includes/masthead.php"); ?>
 
-    <body>
-        <!-- Masthead -->
-        <?php include("../includes/masthead.php"); ?>
+    <!-- Alpha banner -->
+    <?php include("../includes/alpha-banner.php"); ?>
 
-        <!-- Alpha banner -->
-        <?php include("../includes/alpha-banner.php"); ?>
-
-        <!-- Breadcrumbs -->
-        <div class="grid-container">
-            <div class="grid-x grid-padding-x grid-margin-x">
-                <div class="cell">
-                    <div class="govuk-breadcrumbs">
-                        <ol class="govuk-breadcrumbs__list">
-                            <li class="govuk-breadcrumbs__list-item" aria-current="page">All claims: Moorside High School</li>
-                        </ol>
-                    </div>
+    <!-- Breadcrumbs -->
+    <div class="grid-container">
+        <div class="grid-x grid-padding-x grid-margin-x">
+            <div class="cell">
+                <div class="govuk-breadcrumbs">
+                    <ol class="govuk-breadcrumbs__list">
+                        <li class="govuk-breadcrumbs__list-item" aria-current="page">All claims: Moorside High School</li>
+                    </ol>
                 </div>
             </div>
         </div>
+    </div>
 
-        <!-- Main content -->
-        <div class="grid-container">
-            <div id="main-content-area" class="grid-x grid-padding-x grid-margin-x">
-                <div id="main-content-left-only" class="large-8 cell">
+    <!-- Main content -->
+    <div class="grid-container">
+        <div id="main-content-area" class="grid-x grid-padding-x grid-margin-x">
+            <div id="main-content-left-only" class="large-8 cell">
 
-                    <h1 class="govuk-heading-xl">
-                        Retention payment claim details:
-                    </h1>
+                <h1 class="govuk-heading-xl">
+                    Retention payment claim details:
+                </h1>
 
-                    <br/>
+                <br/>
 
-                    <form action="admin-confirmation" method="POST" role="form">
-                        <div class="govuk-form-group">
+                <form action="admin-confirmation" method="POST" role="form">
+                    <div class="govuk-form-group">
 
-                            <input id="admin-claim" name="admin-claim" type="hidden" value="true">
+                        <input id="admin-claim" name="admin-claim" type="hidden" value="true">
 
-                            <!-- only add a heading for a list if there is more than one list -->
-                            <h2 class="govuk-heading-m">Teacher details</h2>
+                        <!-- only add a heading for a list if there is more than one list -->
+                        <h2 class="govuk-heading-m">Teacher details</h2>
 
-                            <!-- use additional modifier class to change column widths for multiple groups of questions and answers: -->
-                            <!--   * `app-check-your-answers--short` for short questions -->
-                            <!--   * `app-check-your-answers__questions-long` for long questions -->
-                            <!--   * none for single group of q&a -->
-                            <dl class="app-check-your-answers app-check-your-answers--short">
+                        <!-- use additional modifier class to change column widths for multiple groups of questions and answers: -->
+                        <!--   * `app-check-your-answers--short` for short questions -->
+                        <!--   * `app-check-your-answers__questions-long` for long questions -->
+                        <!--   * none for single group of q&a -->
+                        <dl class="app-check-your-answers app-check-your-answers--short">
 
-                                <div class="app-check-your-answers__contents">
-                                    <dt class="app-check-your-answers__question">Teacher name</dt>
-                                    <dd class="app-check-your-answers__answer">Jane Jones</dd>
-                                    <dd class="app-check-your-answers__change">&nbsp;</dd>
-                                </div>
+                            <div class="app-check-your-answers__contents">
+                                <dt class="app-check-your-answers__question">Teacher name</dt>
+                                <dd class="app-check-your-answers__answer">Jane Jones</dd>
+                                <dd class="app-check-your-answers__change">&nbsp;</dd>
+                            </div>
 
-                                <div class="app-check-your-answers__contents">
-                                    <dt class="app-check-your-answers__question">Teacher reference number (TRN)</dt>
-                                    <dd class="app-check-your-answers__answer">2316748</dd>
-                                    <dd class="app-check-your-answers__change">&nbsp;</dd>
-                                </div>
+                            <div class="app-check-your-answers__contents">
+                                <dt class="app-check-your-answers__question">Teacher reference number (TRN)</dt>
+                                <dd class="app-check-your-answers__answer">2316748</dd>
+                                <dd class="app-check-your-answers__change">&nbsp;</dd>
+                            </div>
 
-                                <div class="app-check-your-answers__contents">
-                                    <dt class="app-check-your-answers__question">School</dt>
-                                    <dd class="app-check-your-answers__answer">Moorside High School</dd>
-                                    <dd class="app-check-your-answers__change">&nbsp;</dd>
-                                </div>
+                            <div class="app-check-your-answers__contents">
+                                <dt class="app-check-your-answers__question">School</dt>
+                                <dd class="app-check-your-answers__answer">Moorside High School</dd>
+                                <dd class="app-check-your-answers__change">&nbsp;</dd>
+                            </div>
 
-                            </dl>
+                        </dl>
 
-                        </div>
+                    </div>
 
-                        <div class="govuk-form-group ">
+                    <div class="govuk-form-group ">
 
-                            <h2 class="govuk-heading-m">Claim details for Jane Jones</h2>
-
-
-
-                            <dl class="app-check-your-answers wider-titles app-check-your-answers--short">
+                        <h2 class="govuk-heading-m">Claim details for Jane Jones</h2>
 
 
-                                <div class="app-check-your-answers__contents">
+
+                        <dl class="app-check-your-answers wider-titles app-check-your-answers--short">
 
 
-                                    <dt class="app-check-your-answers__question">Did they work at the school between                                         <?php echo $academic_year_start_date ?> and
+                            <div class="app-check-your-answers__contents">
+
+
+                                <dt class="app-check-your-answers__question">Did they work at the school between                                         <?php echo $academic_year_start_date ?> and
                                         <?php echo $academic_year_end_date ?>?</dt>
 
-                                    <?php 
+                                <?php 
     if (!empty($_POST["worked"])) {
   ?>
-                                    <dd class="app-check-your-answers__answer">
-                                        <?php echo $worked ?>
-                                    </dd>
-                                    <?php } else { ?>
-                                    <dd class="app-check-your-answers__answer answer-incomplete">
-                                        Incomplete
-                                    </dd>
-                                    <?php } ?>
-
-                                    <!--      <dd class="app-check-your-answers__answer answer-incomplete">
-                                        <?php echo $worked ?>
-                                    </dd> -->
-
-                                    <dd class="app-check-your-answers__change">
-                                        <button type="submit" class="button-as-link" formaction="admin-confirm-location-eligibility">Update<span class="govuk-visually-hidden"> school eligibility</span></button>
-                                    </dd>
-
-                                </div>
+                                <dd class="app-check-your-answers__answer">
+                                    <?php echo $worked ?>
+                                </dd>
+                                <?php } else { ?>
+                                <dd class="app-check-your-answers__answer answer-incomplete">
+                                    Incomplete
+                                </dd>
+                                <?php } ?>
 
 
-                                <div class="app-check-your-answers__contents">
+                                <dd class="app-check-your-answers__change">
+                                    <button type="submit" class="button-as-link" formaction="admin-confirm-location-eligibility">Update<span class="govuk-visually-hidden"> school eligibility</span></button>
+                                </dd>
+
+                            </div>
+
+
+                            <div class="app-check-your-answers__contents">
 
 
 
-                                    <dt class="app-check-your-answers__question">What subject were they originally employed to teach at Moorside High School?</dt>
+                                <dt class="app-check-your-answers__question">What subject were they originally employed to teach at Moorside High School?</dt>
 
 
-                                    <dd class="app-check-your-answers__answer answer-incomplete">Incomplete</dd>
+                                <dd class="app-check-your-answers__answer answer-incomplete">Incomplete</dd>
 
-                                    <dd class="app-check-your-answers__change">
-                                        <button type="submit" class="button-as-link" formaction="admin-confirm-teaching-eligibility">Update<span class="govuk-visually-hidden"> teaching eligibility</span></button>
-                                    </dd>
+                                <dd class="app-check-your-answers__change">
+                                    <button type="submit" class="button-as-link" formaction="admin-confirm-teaching-eligibility">Update<span class="govuk-visually-hidden"> teaching eligibility</span></button>
+                                </dd>
 
-                                </div>
-
-
-                                <div class="app-check-your-answers__contents">
+                            </div>
 
 
-                                    <dt class="app-check-your-answers__question">Between <?php echo $academic_year_start_date ?> and
+                            <div class="app-check-your-answers__contents">
+
+
+                                <dt class="app-check-your-answers__question">Between <?php echo $academic_year_start_date ?> and
                                         <?php echo $academic_year_end_date ?>, did they spend more than 50% of their time teaching Physics or Maths?</dt>
 
-                                    <dd class="app-check-your-answers__answer answer-incomplete">Incomplete</dd>
+                                <dd class="app-check-your-answers__answer answer-incomplete">Incomplete</dd>
 
-                                    <dd class="app-check-your-answers__change">
-                                        <button type="submit" class="button-as-link" formaction="admin-confirm-phase-eligibility">Update<span class="govuk-visually-hidden"> educational phase</span></button>
-                                    </dd>
+                                <dd class="app-check-your-answers__change">
+                                    <button type="submit" class="button-as-link" formaction="admin-confirm-phase-eligibility">Update<span class="govuk-visually-hidden"> educational phase</span></button>
+                                </dd>
 
-                                </div>
+                            </div>
 
-                            </dl>
+                        </dl>
 
-                        </div>
+                    </div>
 
-                        <h2 class="govuk-heading-m">Confirm the claim</h2>
-                        <p>By submitting this form you are confirming that, to the best of your knowledge, the details you are providing are correct.</p>
-                        <p class="govuk-body">
-                            <button type="submit" class="govuk-button" name="update-claim" value="update">Confirm and send</button>
-                        </p>
+                    <h2 class="govuk-heading-m">Confirm the claim</h2>
+                    <p>By submitting this form you are confirming that, to the best of your knowledge, the details you are providing are correct.</p>
+                    <p class="govuk-body">
+                        <button type="submit" class="govuk-button" name="update-claim" value="update">Confirm and send</button>
+                    </p>
 
-                    </form>
-
-                </div>
-
-                <div id="sidebar" class="large-4 cell no-top-border">
-                    &nbsp;
-                </div>
+                </form>
 
             </div>
+
+            <div id="sidebar" class="large-4 cell no-top-border">
+                &nbsp;
+            </div>
+
         </div>
+    </div>
 
 
-        <!-- Footer -->
-        <?php include("../includes/footer.php"); ?>
+    <!-- Footer -->
+    <?php include("../includes/footer.php"); ?>
 
-        <?php include("../includes/javascript.php"); ?>
+    <?php include("../includes/javascript.php"); ?>
 
-    </body>
+</body>
 
-    </html>
+</html>

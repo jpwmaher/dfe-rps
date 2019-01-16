8dc1914f0a0ef712589b82f21a050377b7bef9db
“Jon <jonmaher@me.com>
2019-01-16 15:11:24 +0000

    s

--- b/teacher-qualified-teach-first.php
+++ b/teacher-qualified-teach-first.php
@@ -6,7 +6,7 @@
 
 <?php include("../includes/variables.php"); ?>
 
-<body class="govuk-template__body js-enabled">
+<body>
     <!-- Masthead -->
     <?php include("../includes/masthead.php"); ?>
 
@@ -18,13 +18,21 @@
         <div id="main-content-area" class="grid-x grid-padding-x grid-margin-x">
             <div id="main-content-left-only" class="large-8 cell">
 
-                <form action="teacher-qualified-itt.php" method="POST" role="form" onsubmit="get_action(this);" data-abide novalidate>
+                <form action="teacher-location-eligibility.php" method="POST" role="form" data-abide novalidate onsubmit="get_action(this);">
+
+                    <div data-abide-error class="alert callout" style="display: none;">
+                        <p><i class="fi-alert"></i>
+                            <?php echo $form_generic_error_message ?>
+                        </p>
+                        <p><a href="#example1Error1">Please enter the subject specialised in during your Teach First initial teacher training?</a></p>
+                    </div>
+
                     <div class="govuk-form-group ">
-                        <fieldset class="govuk-fieldset" role="group" aria-describedby="tbc">
+                        <fieldset class="govuk-fieldset" role="group" aria-describedby="bank-hint">
 
                             <legend class="govuk-fieldset__legend govuk-fieldset__legend--xl">
                                 <h1 class="govuk-fieldset__heading">
-                                    Via which route did you qualify as a teacher?
+                                    What subject did you specialise in during your Teach First initial teacher training?
                                 </h1>
                             </legend>
 
@@ -34,41 +42,37 @@
 
                                     <div class="govuk-radios__item">
 
-                                        <input class="govuk-radios__input" name="x" type="radio" value="teacher-qualified-pgce.php" id="x">
+                                        <input class="govuk-radios__input" name="x" type="radio" value="teacher-qualified-teach-first-ske.php" id="x">
 
-                                        <label class="govuk-label govuk-radios__label" for="check-still-teaching-yes">PGCE</label>
+                                        <label class="govuk-label govuk-radios__label" for="check-still-teaching-yes">Science</label>
                                     </div>
 
                                     <div class="govuk-radios__item">
 
-                                        <input class="govuk-radios__input" name="x" type="radio" value="teacher-qualified-school-direct.php" id="x">
+                                        <input class="govuk-radios__input" name="x" type="radio" value="teacher-location-eligibility.php" id="x">
 
-                                        <label class="govuk-label govuk-radios__label" for="check-still-teaching-yes">School Direct</label>
+                                        <label class="govuk-label govuk-radios__label" for="check-still-teaching-yes">Maths</label>
                                     </div>
 
                                     <div class="govuk-radios__item">
 
-                                        <input class="govuk-radios__input" name="x" type="radio" value="teacher-qualified-scitt.php" id="x">
+                                        <input class="govuk-radios__input" name="x" type="radio" value="teacher-not-eligible.php" id="x">
 
-                                        <label class="govuk-label govuk-radios__label" for="check-still-teaching-yes">SCITT</label>
-                                    </div>
-
-                                    <div class="govuk-radios__item">
-
-                                        <input class="govuk-radios__input" name="x" type="radio" value="teacher-qualified-teach-first.php" id="x">
-
-                                        <label class="govuk-label govuk-radios__label" for="check-still-teaching-no">Teach First</label>
+                                        <label class="govuk-label govuk-radios__label" for="check-still-teaching-yes">Other</label>
                                     </div>
 
                                 </div>
                             </div>
 
                             <div class="govuk-form-group">
+
                                 <button type="submit" class="govuk-button">Continue</button>
+
                             </div>
 
                         </fieldset>
                     </div>
+
                 </form>
 
                 <script type="text/javascript">
@@ -80,7 +84,6 @@
 
             </div>
             <div id="sidebar" class="large-4 cell no-top-border">
-                <!--    <button type="submit" name="skip-verify" value="yes" class="govuk-button hidden-button">Skip Verify</button> -->
             </div>
         </div>
     </div>

1b024453b64336494b9b7cd311a7930e5ac69ff9
“Jon <jonmaher@me.com>
2019-01-21 14:47:49 +0000

    Update summary-footer.php

--- summary/includes/summary-footer.php
+++ summary/includes/summary-footer.php
@@ -1,5 +1,5 @@
-        <div class="grid-x grid-padding-x grid-margin-x">
-            <div id="summary-footer" class="cell">
-                <img src="img/hippo-logo.png" id="hippo-icon">
-            </div>
-        </div>
+<div class="grid-x grid-padding-x grid-margin-x">
+    <div id="summary-footer" class="cell">
+        <a href="hippodigital.co.uk" target="_blank"><img src="img/hippo-logo.png" id="hippo-icon"></a>
+    </div>
+</div>

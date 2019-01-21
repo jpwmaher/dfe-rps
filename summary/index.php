3152bfe0bc2b76ae92492d833a3b478c4fba6baf
“Jon <jonmaher@me.com>
2019-01-21 13:34:42 +0000

    x

--- summary/index.php
+++ summary/index.php
@@ -6,10 +6,12 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SCRa - responsive prototype concepts</title>
+    <title>TSLR - Alpha</title>
     <link rel="stylesheet" href="css/foundation.css">
     <link rel="stylesheet" href="css/app.css">
     <link rel="stylesheet" href="css/scra.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/foundation-icons.css" />
 </head>
 
 <body>

a067b311c4994518672451be5d28c6dd6014ac00
“Jon <jonmaher@me.com>
2019-01-21 14:26:21 +0000

    s

--- summary/index.php
+++ summary/index.php
@@ -15,13 +15,13 @@
         </div>
 
         <div class="grid-x grid-padding-x grid-margin-x">
-            <div id="teacher-icon-div" class="cell" style="margin-top: 40px;">
+            <div id="teacher-icon-div" class="cell">
                 <img src="img/teacher.svg" id="teacher-icon">
             </div>
         </div>
 
         <div class="grid-x grid-padding-x grid-margin-x">
-            <div id="research" class="cell" style="margin-top: 40px;">
+            <div id="research" class="cell" style="margin-top: 80px;">
                 <div class="grid-x grid-padding-x grid-margin-x small-up-1 medium-up-3 large-up-3">
                     <div class="cell nav-panel"><a class="button secondary hollow expanded big-font" href="exec-summary.php">Summary</a></div>
                     <div class="cell nav-panel"><a class="button secondary hollow expanded big-font" href="#">Videos</a></div>

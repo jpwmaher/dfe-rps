e164abaa740dc816615676925b7af765799ba5af
“Jon <jonmaher@me.com>
2019-01-22 15:46:28 +0000

    Update summary.php

--- summary/summary.php
+++ summary/summary.php
@@ -211,7 +211,7 @@
             <div id="what-weve-learned-themes" class="cell" style="text-align: center; margin-top: 120px;" data-magellan-target="what-weve-learned-themes">
                 <br/><br/>
                 <h2>Our first was task to define the <a href="https://docs.google.com/document/d/1wcR7HjW_hTX2sdVLORQ_NCwm9xcw64_cB3IX3T9ER
-                    KU/edit" target="_blank" title="View the full problem statement with links to evidence">problem</a></h2>
+                    KU/edit" target="_blank" title="View the full problem statement with links to evidence">problem</a></h2><br/>
                 <h2>We then gave ourselves the following <a href="https://docs.google.com/document/d/1c3uXEKQnV6ypkOqr8OgoBTu6i_5maARfdMiVoMVzbDw
 /edit" target="_blank" title="View the earlier iterations of the mission statement">mission</a>:</h2><br/><br/>
                 <h4>"Making it quick and easy for teachers to receive a student loan repayment, if they teach a priority subject in an area of greatest need."</h4>

164a66374388a10281d1fd72b39792843c8cbfbb
“Jon <jonmaher@me.com>
2019-01-23 09:53:42 +0000

    Update summary.php

--- summary/summary.php
+++ summary/summary.php
@@ -210,13 +210,11 @@
         <div class="grid-x grid-padding-x grid-margin-x">
             <div class="cell" style="text-align: center; margin-top: 120px;">
                 <br/><br/>
-                <h2>Our first task was to define the <a href="https://docs.google.com/document/d/1wcR7HjW_hTX2sdVLORQ_NCwm9xcw64_cB3IX3T9ER
-                    KU/edit" target="_blank" title="View the full problem statement with links to evidence">problem</a></h2><br/><br/>
+                <h2>Our first task was to define the <a href="https://drive.google.com/open?id=1wcR7HjW_hTX2sdVLORQ_NCwm9xcw64_cB3IX3T9ERKU" target="_blank" title="View the full problem statement with links to evidence">problem</a></h2><br/><br/>
                 <span class="emoji">🤔</span>
                 <br/><br/>
                 <h4 class="standout">"Schools in more challenging areas are finding it harder to recruit and retain teachers, particularly in the subjects of languages (excluding English), science and computer science."</h4><br/><br/><br/><br/><br/><br/><br/>
-                <h2>We then gave ourselves the following <a href="https://docs.google.com/document/d/1c3uXEKQnV6ypkOqr8OgoBTu6i_5maARfdMiVoMVzbDw
-                    /edit" target="_blank" title="View the earlier iterations of the mission statement">mission</a>:</h2>
+                <h2>We then gave ourselves the following <a href="https://drive.google.com/open?id=1c3uXEKQnV6ypkOqr8OgoBTu6i_5maARfdMiVoMVzbDw" target="_blank" title="View the earlier iterations of the mission statement">mission</a>:</h2>
                 <br/><br/>
                 <span class="emoji">🧐</span><br/><br/>
                 <h4 class="standout">"Make it quick and easy for teachers to receive a student loan repayment, if they teach a priority subject in an area of greatest need."</h4><br/><br/><br/><br/><br/><br/><br/>

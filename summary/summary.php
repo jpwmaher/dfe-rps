033c1a17aee31822af9ed348a86694c3ae9bb73d
“Jon <jonmaher@me.com>
2019-01-23 13:23:04 +0000

    Update summary.php

--- summary/summary.php
+++ summary/summary.php
@@ -281,17 +281,6 @@
                             </div>
                         </div>
                     </div>
-                    <div class="large-6 medium-6 cell theme-cell" style="text-align: center;">
-                        <h4 style="text-align: center;">Eligibility</h4>
-                        <p style="text-align: left;">Currently the policy states that a teacher must have been teaching an eligible subject for at least 50% of their time.</p>
-                        <p style="text-align: left;"> Our research suggests asking the teacher what they were employed to teach during the previous tax year instead. This allows us to understand their intent, which aligns better with the policy's objective.
-                        </p>
-                        <div class="grid-x grid-padding-x">
-                            <div class="cell" style="padding: 10%;">
-                                image
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>

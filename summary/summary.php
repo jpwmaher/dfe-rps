481df750d58cf54ec17135a0d50464f7d77e13ef
“Jon <jonmaher@me.com>
2019-01-24 10:50:12 +0000

    Update summary.php

--- summary/summary.php
+++ summary/summary.php
@@ -219,7 +219,7 @@
                 <span class="emoji">🧐</span><br/><br/>
                 <h4 class="standout">"Make it quick and easy for teachers to receive a student loan repayment, if they teach a priority subject in an area of greatest need."</h4><br/><br/><br/><br/><br/><br/><br/>
                 <h2>We then got to work on testing our assumptions...</h2><br/><br/>
-                <span class="emoji">👩🏻‍🏫</span>
+                <span class="emoji">🧪</span>
                 <br/><br/><br/><br/><br/><br/><br/>
             </div>
         </div>

6e0ecbe7143c90e5e2b9cfbc6ce2a6a659fc769e
“Jon <jonmaher@me.com>
2019-01-24 10:47:33 +0000

    s

--- summary/summary.php
+++ summary/summary.php
@@ -261,7 +261,7 @@
                         </p>
                         <div class="grid-x grid-padding-x">
                             <div class="cell" style="padding: 0%;">
-                                <span class="emoji">🏫</span>
+                                <span class="emoji">🧮</span>&nbsp;<img src="img/physics.png">
                             </div>
                         </div>
                     </div>

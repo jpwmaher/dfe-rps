7e3038064ccce781c2f722d3191d41d8acc71398
“Jon <jonmaher@me.com>
2019-01-28 13:32:54 +0000

    Update summary.php

--- summary/summary.php
+++ summary/summary.php
@@ -585,7 +585,7 @@
                     <div class="large-6 medium-6 cell theme-cell" style="text-align: center;">
                         <h4 style="text-align: center;">Physics</h4>
                         <p style="text-align: left;">Proving that 50% of a teachers time was spent teaching Physics isn't an easy task as it's taught as part of Combined Sciences for KS3 &amp; KS4, and teachers are usually employed and contracted as "Teacher of Science".</p>
-                        <p style="text-align: left;">There's also the risk of a number of <a href="#" title="View more details on the potential unitended consequences of the policy">unitended and unwanted consequences</a> of that policy criteria, namely:</p>
+                        <p style="text-align: left;">There's also the risk of a number of <a href="https://docs.google.com/document/d/1QGIFMGfhHFRo-BUlZTHYYR5b8t2-joqyVKXrRZU_eL8/edit#heading=h.37vsrlgnqv6" title="View more details on the potential unintended consequences of the policy">unintended and unwanted consequences</a> of that policy criteria, namely:</p>
 
                         <ul style="text-align: left;">
                             <li>Verification delays</li>
